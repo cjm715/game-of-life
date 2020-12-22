f2658f22ad11464e5e2ec3b8f075c98b94ab7fc0
cjmiles <chris.john.miles@gmail.com>
2020-12-21 23:37:02 -0800

    improved styling

--- main.py
+++ main.py
@@ -13,68 +13,68 @@ import math
 
 
 dataset_test_pd = pd.read_csv('./data/test.csv')
-for delta in range(1,6):
-    dataset_test_pd[dataset_test_pd['delta'] == delta].sort_values('id').to_csv(f"./data/test_delta_{delta}.csv", index = False)
+for delta in range(1, 6):
+    dataset_test_pd[dataset_test_pd['delta'] == delta].sort_values(
+        'id').to_csv(f"./data/test_delta_{delta}.csv", index=False)
 
 sample_sub_df = pd.read_csv(f'./data/sample_submission.csv')
 
-batch_size_desired=5000
+batch_size_desired = 5000
 first_batch = True
 
-for delta in range(5,2,-1):
+for delta in range(5, 2, -1):
     print(delta)
     dataset_test_pd = pd.read_csv(f'./data/test_delta_{delta}.csv')
     dataset_test = TaskDataset(dataset_test_pd)
     dataloader_test = DataLoader(dataset_test, batch_size=batch_size_desired, shuffle=False)
 
-
     for i_batch, sample_batched in enumerate(dataloader_test):
         stop_grid = sample_batched['stop']
         batch_size = stop_grid.size()[0]
-        
-        start_grid_pred = torch.zeros((batch_size,1,25,25), device = device)
-        
+
+        start_grid_pred = torch.zeros((batch_size, 1, 25, 25), device=device)
+
         cost = accuracy_error(step(start_grid_pred, delta=delta)[0], stop_grid)
         cost_best = cost.clone()
         print(cost_best.mean())
         start_grid_pred_best = start_grid_pred.clone()
-        pbar = tqdm(torch.arange(360000, device = device))
+        pbar = tqdm(torch.arange(360000, device=device))
         temperature = 0.1
         gamma = 0.999
         for i in pbar:
             start_grid_pred_new = start_grid_pred.clone()
-            
+
             if i % 500 == 0:
                 start_grid_pred_new = start_grid_pred_best.clone()
-                
-            start_grid_pred_new = flip_n_bits(start_grid_pred_new, 1, batch_size)   
+
+            start_grid_pred_new = flip_n_bits(start_grid_pred_new, 1, batch_size)
             stop_grid_pred_new, _ = step(start_grid_pred_new, delta=delta)
             cost_new = accuracy_error(stop_grid_pred_new, stop_grid)
 
-            cost_change = cost_new - cost #+ reg
+            cost_change = cost_new - cost  # + reg
 
             temperature = max(temperature*gamma, 1e-4)
-            
-            prob,_ = torch.min(torch.exp(-cost_change/temperature),1)
-            choice = (torch.rand(batch_size, device = device) < prob)
-            start_grid_pred = torch.where(choice.view(batch_size, 1, 1, 1), 
-                                         start_grid_pred_new,
-                                         start_grid_pred)
+
+            prob, _ = torch.min(torch.exp(-cost_change/temperature), 1)
+            choice = (torch.rand(batch_size, device=device) < prob)
+            start_grid_pred = torch.where(choice.view(batch_size, 1, 1, 1),
+                                          start_grid_pred_new,
+                                          start_grid_pred)
             cost = torch.where(choice.view(batch_size, 1), cost_new, cost)
-            
+
             start_grid_pred_best = torch.where(
-                (cost < cost_best).view(batch_size, 1, 1, 1), 
-                start_grid_pred, 
+                (cost < cost_best).view(batch_size, 1, 1, 1),
+                start_grid_pred,
                 start_grid_pred_best)
             cost_best = torch.where(cost < cost_best, cost, cost_best)
 
             pbar.set_description(f' Cost (best): {cost_best.mean()} Temp : {temperature}')
 
-        start_grid_pred_best_np = start_grid_pred_best.cpu().numpy().reshape(batch_size,SIDE_LENGTH*SIDE_LENGTH)
+        start_grid_pred_best_np = start_grid_pred_best.cpu().numpy().reshape(batch_size, SIDE_LENGTH*SIDE_LENGTH)
         sub_df = pd.DataFrame(start_grid_pred_best_np)
         sub_df.insert(loc=0, column='id', value=sample_batched['id'].cpu().numpy())
         sub_df.columns = sample_sub_df.columns
-        
+
         if first_batch:
             full_sub_df = sub_df.copy()
             cost_best_all = cost_best.clone()
@@ -82,7 +82,6 @@ for delta in range(5,2,-1):
         else:
             full_sub_df = pd.concat([full_sub_df, sub_df])
             cost_best_all = torch.cat([cost_best_all, cost_best])
-              
-        print(cost_best_all.mean())   
+
+        print(cost_best_all.mean())
         full_sub_df.sort_values('id').to_csv('submission.csv', index=False)
-        
--- model.py
+++ model.py
@@ -12,12 +12,13 @@ import sys
 SIDE_LENGTH = 25
 NUM_WARM_UP_STEPS = 5
 GRID_SHAPE = (SIDE_LENGTH, SIDE_LENGTH)
-device="cuda"
+device = "cuda"
 
 TOTAL_NUM_CELLS = torch.tensor(SIDE_LENGTH*SIDE_LENGTH).type(torch.LongTensor).to(device)
-MASK = torch.tensor([[1,1,1],
-                     [1,0,1],
-                     [1,1,1]]).view(1,1,3,3).type(torch.FloatTensor).to(device)
+MASK = torch.tensor([[1, 1, 1],
+                     [1, 0, 1],
+                     [1, 1, 1]]).view(1, 1, 3, 3).type(torch.FloatTensor).to(device)
+
 
 def line2grid_tensor(data, device='cuda'):
     grid = data.to_numpy().reshape((data.shape[0], 1, 25, 25))
@@ -25,15 +26,15 @@ def line2grid_tensor(data, device='cuda'):
 
 
 class TaskDataset(Dataset):
-    def __init__(self, data, data_initialize = None, device='cuda'):
-        self.id = LongTensor(data.iloc[:,0].to_numpy()).to(device)
-        self.delta = LongTensor(data.iloc[:,1].to_numpy()).to(device)
-        self.stop = line2grid_tensor(data.iloc[:,2:], device)
+    def __init__(self, data, data_initialize=None, device='cuda'):
+        self.id = LongTensor(data.iloc[:, 0].to_numpy()).to(device)
+        self.delta = LongTensor(data.iloc[:, 1].to_numpy()).to(device)
+        self.stop = line2grid_tensor(data.iloc[:, 2:], device)
         if data_initialize is not None:
-            self.start_guess = line2grid_tensor(data_initialize.iloc[:,1:], device)
+            self.start_guess = line2grid_tensor(data_initialize.iloc[:, 1:], device)
         else:
             self.start_guess = None
-        
+
     def __len__(self):
         return len(self.delta)
 
@@ -48,37 +49,46 @@ def G(s, grid):
     sumIs2 = s == 2
     sumIs3 = s == 3
     isAlive = grid == 1
-    grid = torch.logical_or(torch.logical_and(torch.logical_or(sumIs2, sumIs3), isAlive),
-                         torch.logical_and(sumIs3, torch.logical_not(isAlive)))
+    grid = torch.logical_or(
+        torch.logical_and(torch.logical_or(sumIs2, sumIs3), isAlive),
+        torch.logical_and(sumIs3, torch.logical_not(isAlive)))
     grid = grid.type(torch.int)
     return grid
 
+
 def sum_neighbors(grid):
-    grid_padded = F.pad(grid,(1,1,1,1), mode="circular").type(torch.cuda.FloatTensor)
+    grid_padded = F.pad(grid, (1, 1, 1, 1), mode="circular").type(torch.cuda.FloatTensor)
     s_layer = F.conv2d(grid_padded, MASK).type(torch.cuda.IntTensor)
     return s_layer
+
 
 def step_single(grid):
     s_layer = sum_neighbors(grid)
     grid = G(s_layer, grid)
     return grid, s_layer
 
+
 def step(grid, delta=1):
-    for _ in range(0,delta):
-        grid,s_layer = step_single(grid)
+    for _ in range(0, delta):
+        grid, s_layer = step_single(grid)
     return grid, s_layer
 
+
 def accuracy_error(grid_pred, grid_true):
-    C = torch.sum(grid_pred != grid_true, (2,3)).type(torch.cuda.FloatTensor)/TOTAL_NUM_CELLS
+    C = torch.sum(grid_pred != grid_true, (2, 3)).type(
+        torch.cuda.FloatTensor)/TOTAL_NUM_CELLS
     return C
 
-def flip_n_bits(grid,n,batch_size):
-    flat_index_per_frame = torch.randint(0,SIDE_LENGTH*SIDE_LENGTH, (batch_size,n))
-    total_cell_upto_last_frame = torch.arange(0, SIDE_LENGTH*SIDE_LENGTH*batch_size, SIDE_LENGTH*SIDE_LENGTH).view(batch_size,1)
+
+def flip_n_bits(grid, n, batch_size):
+    flat_index_per_frame = torch.randint(0, SIDE_LENGTH*SIDE_LENGTH, (batch_size, n))
+    total_cell_upto_last_frame = torch.arange(
+        0, SIDE_LENGTH*SIDE_LENGTH*batch_size, SIDE_LENGTH*SIDE_LENGTH).view(batch_size, 1)
     flat_indices = total_cell_upto_last_frame + flat_index_per_frame
-    flat_indices=flat_indices.squeeze()
-    grid.flatten()[flat_indices] = 1 - grid.flatten()[flat_indices] 
+    flat_indices = flat_indices.squeeze()
+    grid.flatten()[flat_indices] = 1 - grid.flatten()[flat_indices]
     return grid
+
 
 def plotg(grid):
     plt.imshow(grid, cmap='Greys',  interpolation='nearest')
